4ddc9ce792014b853a947f782c6afa18c645d59c
Adrian Galera <agalera@nicepeopleatwork.com>
2014-01-07 13:24:14 +0100

    Range

--- timestamps.py
+++ timestamps.py
@@ -20,13 +20,15 @@ if options.date:
 	sys.exit(0);
 
 else:
-
 	if len(sys.argv) == 2:
 		timestampString = sys.argv[1];
+		timestampString = timestampString.split("-");
+		
 		#Etc/GMT have the reversed symbol, GMT-1 actually is GMT+1
 		timezone = timezone("Etc/GMT-1");
 	elif len(sys.argv) == 3:
 		timestampString = sys.argv[1];
+		timestampString = timestampString.split("-");
 		tzString = sys.argv[2];
 		timeZoneString ="Etc/GMT";
 		#Etc/GMT have the reversed symbol, GMT-1 actually is GMT+1
@@ -42,26 +44,37 @@ else:
 		sys.exit(0);
 
 
-	l = len(timestampString);
+	l = len(timestampString[0]);
 		
 
-
 	if l==10:
-		date = datetime.fromtimestamp(int(timestampString),timezone);
-		print date.strftime('%Y-%m-%d %H:%M:%S %z');
+		dateSt="";
+		for d in timestampString:
+			date = datetime.fromtimestamp(int(d),timezone);
+			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";
+		print dateSt;
 
 
 	elif l==13:
-		date = datetime.fromtimestamp(int(timestampString)/1e3,timezone);
-		print date.strftime('%Y-%m-%d %H:%M:%S %z');
+		dateSt="";
+		for d in timestampString:
+			date = datetime.fromtimestamp(int(d)/1e3,timezone);
+			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";
+		print dateSt;
 
 	elif l==8:
-		date = datetime.fromtimestamp(int(timestampString)*60,timezone);
-		print date.strftime('%Y-%m-%d %H:%M:%S %z');
+		dateSt="";
+		for d in timestampString:
+			date = datetime.fromtimestamp(int(d)*60,timezone);
+			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";
+		print dateSt;
 
 	elif l==6:
-		date = datetime.fromtimestamp(int(timestampString)*3600,timezone);
-		print date.strftime('%Y-%m-%d %H:%M:%S %z');	
+		dateSt="";
+		for d in timestampString:
+			date = datetime.fromtimestamp(int(d)*3600,timezone);
+			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+" \t";
+		print dateSt;
 
 	else:
 		print("Timestamp format not correctly detected");
