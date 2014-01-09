55452e584c21bf364e9aea54d64f802f2662460e
Adrian Galera <agalera@nicepeopleatwork.com>
2014-01-09 16:20:58 +0100

    Added support for JSON timestamp parsing

--- timestamps.py
+++ timestamps.py
@@ -6,10 +6,40 @@ import pytz
 from optparse import OptionParser
 from dateutil import parser
 import time
+import simplejson as json
+
+def parseTimestampString(timestampString,timezone):
+	dateSt="";
+	for d in timestampString:
+		l = len(d);
+
+		if l==10:
+			date = datetime.fromtimestamp(int(d),timezone);
+			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";		
+
+		elif l==13:
+			date = datetime.fromtimestamp(int(d)/1e3,timezone);
+			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";
+
+		elif l==8:
+			date = datetime.fromtimestamp(int(d)*60,timezone);
+			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";
+
+		elif l==6:
+			date = datetime.fromtimestamp(int(d)*3600,timezone);
+			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+" \t";
+
+		else:
+			print("Timestamp format not correctly detected "+d);
+			sys.exit(0);
+
+	return dateSt;
 
 op_parser = OptionParser()
 op_parser.add_option("-c", "--calculate-date", dest="date",
                   help="Calculate the timestamp from the date specified");
+op_parser.add_option("-j", "--json", dest="json",
+                  help="Return the conversed dates from the specified json timestamps");
 
 (options, args) = op_parser.parse_args()
 
@@ -17,6 +47,37 @@ if options.date:
 	date = parser.parse(options.date)
 	unix_time = int(time.mktime(date.timetuple()))
 	print unix_time
+	sys.exit(0);
+
+if options.json:
+	#Forced GMT+0
+	timezone = timezone("GMT+0");
+	jsonObj = json.loads(options.json);
+	timestampString="";
+	try:
+		for timestampPair in jsonObj[""]["LIVE"]:
+			timestampString+=str(timestampPair[0])+"-";
+	except:
+		print "No LIVE values";
+	
+	if timestampString:
+		timestampString = timestampString.split("-");
+		print "************** LIVE TS ***********************\n"
+		print parseTimestampString(timestampString[0:-1],timezone)+"\n";
+		print "************** LIVE TS ***********************\n"
+
+	timestampString="";
+	try:
+		for timestampPair in jsonObj[""]["VOD"]:
+			timestampString+=str(timestampPair[0])+"-";
+	except:
+		print "No VOD values";
+	if timestampString:
+		timestampString = timestampString.split("-");
+		print "************** VOD TS ***********************\n"
+		print parseTimestampString(timestampString[0:-1],timezone)+"\n";
+		print "************** VOD TS ***********************\n"
+
 	sys.exit(0);
 
 else:
@@ -43,30 +104,7 @@ else:
 		print("Wrong number of parameters");
 		sys.exit(0);
 
-	dateSt="";
-	for d in timestampString:
-		l = len(d);
-		
-
-		if l==10:
-			date = datetime.fromtimestamp(int(d),timezone);
-			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";		
+	print parseTimestampString(timestampString,timezone);
 
 
-		elif l==13:
-			date = datetime.fromtimestamp(int(d)/1e3,timezone);
-			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";
 
-		elif l==8:
-			date = datetime.fromtimestamp(int(d)*60,timezone);
-			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+"\t";
-
-		elif l==6:
-			date = datetime.fromtimestamp(int(d)*3600,timezone);
-			dateSt += date.strftime('%Y-%m-%d %H:%M:%S %z')+" \t";
-
-		else:
-			print("Timestamp format not correctly detected");
-			sys.exit(0);
-
-	print dateSt;
